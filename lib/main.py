1a8072cb925d9b5e73804d496144ed0e3d13ddac
amassock <ajmassock@gmail.com>
2023-04-15 14:20:49 -0500

    move to functions

--- lib/main.py
+++ lib/main.py
@@ -23,7 +23,8 @@ class Note(BaseModel):
     date_added = DateTimeField(default=datetime.datetime.now)
 
 # Drop table if exists
-db.drop_tables([Note])
+def remove_all():
+    db.drop_tables([Note])
 
 # Create table
 db.create_tables([Note])
@@ -32,5 +33,19 @@ new_note = Note(title='Test', body='Testing of db connection')
 new_note.save()
 
 # List all notes
-list_all = Note.select()
-print([[note.title, note.body] for note in list_all])
+def list_all():
+    list_all = Note.select()
+    print([[note.title, note.body] for note in list_all])
+
+# List one note based off title
+
+# Create new note
+def create_new():
+    title = input('What is the title? ')
+    body = input('What is your note? ')
+    full_note = Note(title=title, body=body)
+    full_note.save()
+
+# Remove note based off title
+
+# Update note based off title
